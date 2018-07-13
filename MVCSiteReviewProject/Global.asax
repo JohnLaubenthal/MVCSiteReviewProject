1ac071c16725349b3135e0d93fd4bee0701dedeb
JohnLaubenthal <laubenthaljohn@yahoo.com>
2018-07-13 10:58:35 -0400

    first commit

---
+++ MVCSiteReviewProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCSiteReviewProject.MvcApplication" Language="C#" %>
---
+++ MVCSiteReviewProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCSiteReviewProject.Startup))]
+namespace MVCSiteReviewProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
