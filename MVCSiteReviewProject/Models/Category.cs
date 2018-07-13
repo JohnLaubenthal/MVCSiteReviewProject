e3bac76866ccded4025c7f554d218574da2f14c3
JohnLaubenthal <laubenthaljohn@yahoo.com>
2018-07-13 11:45:34 -0400

    ReviewModelPopulated

---
+++ MVCSiteReviewProject/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCSiteReviewProject.Models
+{
+    public class Category
+    {
+    }
+}
---
+++ MVCSiteReviewProject/Models/Review.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCSiteReviewProject.Models
+{
+    public class Review
+    {
+        [Key]
+        public int ReviewId { get; set; }
+        public string ReviewTitle { get; set; }
+        public string Content { get; set; }
+        public DateTime ReviewDate { get; set; }
+    }
+}
